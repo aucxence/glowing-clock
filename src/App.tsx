1df5ddce00f4bb3f4e4e31e13087f5f2271d80ff
aucxence <aucxence@yahoo.fr>
2020-12-18 22:22:45 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const updateClock = () => {
+    // console.log('---------------------');
+    var now = new Date();
+    var dname = now.getDay(),
+      mo = now.getMonth(),
+      dnum = now.getDate(),
+      yr: any = now.getFullYear(),
+      hou = now.getHours(),
+      min = now.getMinutes(),
+      sec = now.getSeconds(),
+      pe = "AM";
+
+    if (hou >= 12) {
+      pe = "PM";
+    }
+    if (hou == 0) {
+      hou = 12;
+    }
+    if (hou > 12) {
+      hou = hou - 12;
+    }
+
+    const pad = (num: number, size: number): string => {
+      let s = num + "";
+      while (s.length < size) s = "0" + s;
+      return s;
+    }
+
+    var months = ["January", "February", "March", "April", "May", "June", "July", "Augest", "September", "October", "November", "December"];
+    var week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    console.log([week[dname], months[mo], pad(dnum, 2), yr, pad(hou, 2), pad(min, 2), pad(sec, 2), pe]);
+
+    setDname(week[dname]);
+    setMonth(months[mo]);
+    setDay(pad(dnum, 2));
+    setYear(yr);
+
+    setHour(pad(hou, 2));
+    setMinute(pad(min, 2));
+    setSecond(pad(sec, 2));
+    setPe(pe);
+
+    return [week[dname], months[mo], pad(dnum, 2), yr, pad(hou, 2), pad(min, 2), pad(sec, 2), pe];
+  }
+
+  useEffect(() => {
+    updateClock();
+    setInterval(() => updateClock(), 500);
+  }, []);
+
+  const [dname, setDname] = useState('');
+  const [day, setDay] = useState('');
+  const [month, setMonth] = useState('');
+  const [year, setYear] = useState('');
+
+  const [hour, setHour] = useState('');
+  const [minute, setMinute] = useState('');
+  const [second, setSecond] = useState('');
+  const [pe, setPe] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <!--digital clock start--> */}
+      <div className="datetime">
+        <div className="date">
+          <span id="dayname">{dname + ', '}</span>
+          <span id="month">{month + ', '}</span>
+          <span id="daynum">{day + ', '}</span>
+          <span id="year">{year}</span>
+        </div>
+        <div className="time">
+          <span id="hour">{hour}</span>:
+          <span id="minutes">{minute}</span>:
+          <span id="seconds">{second}</span>
+          <span id="period">{pe}</span>
+        </div>
+      </div>
+      {/* <!--digital clock end--> */}
     </div>
   );
 }
